b376e8886609d7fbf06a1788178f2e6acfaaeef3
JeremAIh <esiebomaj@gmail.com>
2021-09-22 21:15:43 +0100

    added auth on client side

--- react-web/src/components/dashboard/TopBar.jsx
+++ react-web/src/components/dashboard/TopBar.jsx
@@ -1,6 +1,19 @@
 import { AiOutlineSearch } from "react-icons/ai";
+import { Auth, Hub } from "aws-amplify";
 
 const TopBar = () => {
+  const handleSignOutButtonClick = async () => {
+    try {
+      await Auth.signOut();
+      Hub.dispatch("UI Auth", {
+        event: "AuthStateChange",
+        message: "signedout",
+      });
+    } catch (error) {
+      console.log("error signing out: ", error);
+    }
+  };
+
   return (
     <div className="top-bar">
       <div className="search-form">
@@ -8,6 +21,9 @@ const TopBar = () => {
         <input type="text" placeholder="Search" />
       </div>
       <div className="user">
+        <button className="signout" onClick={handleSignOutButtonClick}>
+          Sign out
+        </button>
         <div>Hello Nick</div>
         <img className="profile-img" src="/avatar.png" alt="profile img" />
       </div>
